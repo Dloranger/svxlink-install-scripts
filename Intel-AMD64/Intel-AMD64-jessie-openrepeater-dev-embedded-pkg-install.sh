9cb0004bb7bd1cc6513a426920e95a0ca3ff7095
Richard Neese <r.neese@gmail.com>
2015-10-26 11:41:34 -0500

    svxlink install scripts

---
+++ Intel-AMD64/Intel-AMD64-jessie-openrepeater-dev-embedded-pkg-install.sh
@@ -0,0 +1,399 @@
+#!/bin/bash
+(
+###################################################################
+#   Open Repeater Project
+#
+#    Copyright (C) <2015>  <Richard Neese> kb3vgw@gmail.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.
+#
+#    If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>
+#
+###################################################################
+# Auto Install Configuration options 
+# (set it, forget it, run it)
+###################################################################
+
+# ----- Start Edit Here ----- #
+####################################################
+# Repeater call sign
+# Please change this to match the repeater call sign
+####################################################
+cs="Set-This"
+
+###################################################
+# Put /var/log into a tmpfs to improve performance 
+# Super user option dont try this if you must keep 
+# logs after every reboot
+###################################################
+put_logs_tmpfs="n"
+
+# ----- Stop Edit Here ------- #
+
+######################################################################
+# check to see that the configuration portion of the script was edited
+######################################################################
+if [[ $cs == "Set-This" ]]; then
+  echo
+  echo "Looks like you need to configure the scirpt before running"
+  echo "Please configure the script and try again"
+  exit 0
+fi
+
+##################################################################
+# check to confirm running as root. # First, we need to be root...
+##################################################################
+if [ "$(id -u)" -ne "0" ]; then
+  sudo -p "$(basename "$0") must be run as root, please enter your sudo password : " "$0" "$@"
+  exit 0
+fi
+echo
+echo "Looks Like you are root.... continuing!"
+echo
+
+###############################################
+#if lsb_release is not installed it installs it
+###############################################
+if [ ! -s /usr/bin/lsb_release ]; then
+	apt-get update && apt-get -y install lsb-release
+fi
+
+#################
+# Os/Distro Check
+#################
+lsb_release -c |grep -i jessie &> /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	echo " OK you are running Debian 8 : Jessie "
+else
+	echo " This script was written for Debian 8 Jessie "
+	echo
+	echo " Your OS appears to be: " lsb_release -a
+	echo
+	echo " Your OS is not currently supported by this script ... "
+	echo
+	echo " Exiting the install. "
+	exit
+fi
+
+###########################################
+# Run a OS and Platform compatabilty Check
+###########################################
+########
+# ARMEL
+########
+case $(uname -m) in armv[4-5]l)
+echo
+echo " ArmEL is currenty UnSupported "
+echo
+exit
+esac
+
+########
+# ARMHF
+########
+case $(uname -m) in armv[6-9]l)
+echo
+echo " ArmHF arm v7 v8 v9 boards supported "
+echo
+esac
+
+#############
+# Intel/AMD
+#############
+case $(uname -m) in x86_64|i[4-6]86)
+echo
+echo " Intel / Amd boards currently Support is comming soon "
+echo
+exit
+esac
+
+###################
+# Notes / Warnings
+###################
+echo
+cat << DELIM
+                   Not Ment For L.a.m.p Installs
+
+                  L.A.M.P = Linux Apache Mysql PHP
+
+                 THIS IS A ONE TIME INSTALL SCRIPT
+
+             IT IS NOT INTENDED TO BE RUN MULTIPLE TIMES
+
+         This Script Is Ment To Be Run On A Fresh Install Of
+
+                         Debian 8 (Jessie)
+
+     If It Fails For Any Reason Please Report To kb3vgw@gmail.com
+
+   Please Include Any Screen Output You Can To Show Where It Fails
+   
+  Note:
+
+  Pre-Install Information:
+
+       This script uses Sqlite by default. No plans to use Other DB. 
+
+DELIM
+
+###############################################################################################
+#Testing for internet connection. Pulled from and modified
+#http://www.linuxscrew.com/2009/04/02/tiny-bash-scripts-check-internet-connection-availability/
+###############################################################################################
+echo
+echo "This Script Currently Requires a internet connection "
+echo
+wget -q --tries=10 --timeout=5 http://www.google.com -O /tmp/index.google &> /dev/null
+
+if [ ! -s /tmp/index.google ];then
+	echo "No Internet connection. Please check ethernet cable"
+	/bin/rm /tmp/index.google
+	exit 1
+else
+	echo "I Found the Internet ... continuing!!!!!"
+	/bin/rm /tmp/index.google
+fi
+echo
+printf ' Current ip is : '; ip -f inet addr show dev eth0 | sed -n 's/^ *inet *\([.0-9]*\).*/\1/p'
+echo
+
+######################################
+# Reconfigure system for performance
+######################################
+##############################
+#Set a reboot if Kernel Panic
+##############################
+cat > /etc/sysctl.conf << DELIM
+kernel.panic = 10
+DELIM
+
+####################################
+# Set fs to run in a tempfs ramdrive
+####################################
+cat >> /etc/fstab << DELIM
+tmpfs /tmp  tmpfs nodev,nosuid,mode=1777  0 0
+tmpfs /var/tmp  tmpfs nodev,nosuid,mode=1777  0 0
+tmpfs /var/cache/apt/archives tmpfs   size=100M,defaults,noexec,nosuid,nodev,mode=0755 0 0
+DELIM
+
+
+
+######################
+# Enable the spi & i2c
+######################
+echo "spicc" >> /etc/modules
+echo "aml_i2c" >> /etc/modules
+
+##########################################
+# SETUP configuration for /tmpfs for logs
+##########################################
+if [[ $put_logs_tmpfs == "y" ]]; then
+#################
+#configure fstab
+#################
+cat >>/etc/fstab << DELIM
+tmpfs   /var/log                tmpfs   size=20M,defaults,noatime,mode=0755 0 0 
+DELIM
+
+########################
+# cnfigure tmpfs sizes
+########################
+cp /etc/default/tmpfs /etc/default/tmpfs.orig
+cat > /etc/default/tmpfs << DELIM
+RAMLOCK=yes
+RAMSHM=yes
+RAMTMP=yes
+
+TMPFS_SIZE=10%VM
+RUN_SIZE=10M
+LOCK_SIZE=5M
+SHM_SIZE=10M
+TMP_SIZE=25M
+
+DELIM
+
+#######################################
+# Configure /var/log dir's on reboots
+#######################################
+cat > /etc/init.d/preplog-dirs << DELIM
+#!/bin/bash
+#
+### BEGIN INIT INFO
+# Provides:          prepare-dirs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Required-Start:
+# Required-Stop:
+# Short-Description: Create needed directories on /var/log/ for tmpfs at startup
+# Description:       Create needed directories on /var/log/ for tmpfs at startup
+### END INIT INFO
+# needed Dirs
+DIR[0]=/var/log/nginx
+DIR[1]=/var/log/apt
+DIR[2]=/var/log/ConsoleKit
+DIR[3]=/var/log/fsck
+DIR[4]=/var/log/news
+DIR[5]=/var/log/ntpstats
+DIR[6]=/var/log/samba
+DIR[7]=/var/log/lastlog
+DIR[8]=/var/log/exim
+DIR[9]=/var/log/watchdog
+case "${1:-''}" in
+  start)
+        typeset -i i=0 max=${#DIR[*]}
+        while (( i < max ))
+        do
+                mkdir  ${DIR[$i]}
+                chmod 755 ${DIR[$i]}
+                i=i+1
+        done
+        # set rights
+        chown www-data.adm ${DIR[0]}
+        chown root.adm ${DIR[6]}
+    ;;
+  stop)
+    ;;
+  restart)
+   ;;
+  reload|force-reload)
+   ;;
+  status)
+   ;;
+  *)
+DELIM
+
+chmod 755 /etc/init.d/preplog-dirs
+
+fi
+
+#############################
+#Setting Host/Domain name
+#############################
+cat > /etc/hostname << DELIM
+$cs-repeater
+DELIM
+
+#################
+#Setup /etc/hosts
+#################
+cat > /etc/hosts << DELIM
+127.0.0.1       localhost
+::1             localhost ip6-localhost ip6-loopback
+fe00::0         ip6-localnet
+ff00::0         ip6-mcastprefix
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+
+127.0.0.1       $cs-repeater
+DELIM
+
+#################################################################################################
+# Setting apt_get to use the httpredirecter to get
+# To have <APT> automatically select a mirror close to you, use the Geo-ip redirector in your
+# sources.list "deb http://httpredir.debian.org/debian/ jessie main".
+# See http://httpredir.debian.org/ for more information.  The redirector uses HTTP 302 redirects
+# not dnS to serve content so is safe to use with Google dnS.
+# See also <which httpredir.debian.org>.  This service is identical to http.debian.net.
+#################################################################################################
+cat > "/etc/apt/sources.list" << DELIM
+deb http://httpredir.debian.org/debian/ jessie main contrib non-free
+#deb-src http://httpredir.debian.org/debian/ jessie main contrib non-free
+
+deb http://httpredir.debian.org/debian/ jessie-updates main contrib non-free
+#deb-src http://httpredir.debian.org/debian/ jessie-updates main contrib non-free
+
+deb http://httpredir.debian.org/debian/ jessie-backports main contrib non-free
+#deb-src http://httpredir.debian.org/debian/ jessie-backports main contrib non-free
+
+DELIM
+
+####################
+# Odroid c1 c1+ repo
+####################
+cat > /etc/apt/sources.list.d/odroid.list << DELIM
+deb http://deb.odroid.in/c1/ trusty main
+deb http://deb.odroid.in/ trusty main
+DELIM
+
+##########################
+# Adding OpenRepeater Repo
+##########################
+cat > "/etc/apt/sources.list.d/openrepeater.list" <<DELIM
+deb http://repo.openrepeater.com/openrepeater/devel/debian/ jessie main
+DELIM
+
+#########################
+# SVXLink Testing repo
+#########################
+cat > "/etc/apt/sources.list.d/svxlink.list" <<DELIM
+deb http://repo.openrepeater.com/svxlink-dev/devel/debian/ jessie main
+DELIM
+
+######################
+#Update base os
+######################
+for i in update upgrade clean ;do apt-get -y "${i}" ; done
+
+######################
+#Install Dependancies
+#####################
+apt-get install -y --force-yes memcached sqlite3 libopus0 alsa-utils vorbis-tools sox libsox-fmt-mp3 librtlsdr0 \
+		ntp libasound2 libspeex1 libgcrypt20 libpopt0 libgsm1 tcl8.6 alsa-base bzip2 sudo gpsd gpsd-clients \
+		flite wvdial inetutils-syslogd screen time uuid vim install-info usbutils whiptail dialog logrotate cron \
+		gawk watchdog python3-serial
+
+#####################
+# Install SvxLink
+#####################		
+apt-get install -y --force-yes svxlink-server remotetrx 
+
+#############################################
+#making links to make svxlink work correctly
+#############################################
+ln -s /etc/svxlink/local-events.d/ /usr/share/svxlink/events.d/local
+
+#Working on sounds pkgs for future release of svxlink
+cd /usr/share/svxlink/sounds
+wget https://github.com/sm0svx/svxlink-sounds-en_US-heather/releases/download/14.08/svxlink-sounds-en_US-heather-16k-13.12.tar.bz2
+tar xjvf svxlink-sounds-en_US-heather-16k-13.12.tar.bz2
+mv en_US-heather* en_US
+rm svxlink-sounds-en_US-heather-16k-13.12.tar.bz2
+cd /root
+
+
+##################################
+# Enable New shellmenu for logins
+# on enabled for root and only if 
+# the file exist
+##################################
+cat >> /root/.profile << DELIM
+
+if [ -f /usr/bin/odroid-svxlink-conf ]; then
+        . /usr/bin/svxlink-conf
+fi
+
+DELIM
+
+echo " ########################################################################################## "
+echo " #             The SVXLink Repeater / Echolink server Install is now complete             # "
+echo " #                          and your system is ready for use..                            # "
+echo " #                                                                                        # "
+echo " #                   To Start the service fo svxlink on the cmd line                      # "
+echo " #                        run cmd: systemctl enable svxlink.service                       # "
+echo " #                                                                                        # "
+echo " #                   To Start the service fo remotetrx on the cmd line                    # "
+echo " #                        run cmd: systemctl enable remotetrx.service                     # "
+echo " #                                                                                        # "
+echo " ########################################################################################## "
+) | tee /root/install.log

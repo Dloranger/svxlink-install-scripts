8d636cfb437cd4750a0c462b31614ad82b52bb09
Richard Neese <r.neese@gmail.com>
2016-01-13 15:51:02 -0600

    fixed missing wget

--- RELEASE/RaspberryPi/Raspi2-RaspBian-Jessie-SvxLink-Release-embedded-pkg-install.sh
+++ RELEASE/RaspberryPi/Raspi2-RaspBian-Jessie-SvxLink-Release-embedded-pkg-install.sh
@@ -318,7 +318,7 @@ rm svxlink-sounds-en_US-heather-16k-15.11.2.tar.bz2
 ##############################
 #Install Courtesy Sound Files
 ##############################
-https://github.com/kb3vgw/Svxlink-Courtesy_Tones/archive/15.10.tar.gz
+wget https://github.com/kb3vgw/Svxlink-Courtesy_Tones/archive/15.10.tar.gz
 tar xjvf 15.10.tar.gz
 mv Svxlink-Courtesy_Tones-15.10 Courtesy_Tones
 mv Courtesy_Tones /usr/share/svxlink/sounds/

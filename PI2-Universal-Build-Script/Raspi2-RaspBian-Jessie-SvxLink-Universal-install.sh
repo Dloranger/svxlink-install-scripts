96a325a81ed4614f04c7ae4d787c15dd37ff80e9
Richard Neese <r.neese@gmail.com>
2016-01-22 10:48:29 -0600

    new script

---
+++ PI2-Universal-Build-Script/Raspi2-RaspBian-Jessie-SvxLink-Universal-install.sh
@@ -0,0 +1,385 @@
+#!/bin/bash
+(
+#######################################
+# Auto Install Configuration options
+# (set it, forget it, run it)
+#######################################
+######################################################################
+# check to see that the configuration portion of the script was edited
+######################################################################
+if [[ $cs == "Set-This" ]]; then
+  echo
+  echo "Looks like you need to configure the scirpt before running"
+  echo "Please configure the script and try again"
+  exit 0
+fi
+
+##################################################################
+# check to confirm running as root. # First, we need to be root...
+##################################################################
+if [ "$(id -u)" -ne "0" ]; then
+  sudo -p "$(basename "$0") must be run as root, please enter your sudo password : " "$0" "$@"
+  exit 0
+fi
+echo
+echo "Looks Like you are root.... continuing!"
+echo
+
+###############################################
+#if lsb_release is not installed it installs it
+###############################################
+if [ ! -s /usr/bin/lsb_release ]; then
+	apt-get update && apt-get -y install lsb-release
+fi
+
+#################
+# Os/Distro Check
+#################
+lsb_release -c |grep -i jessie &> /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	echo " OK you are running Debian 8 : Jessie "
+else
+	echo " This script was written for Debian 8 Jessie "
+	echo
+	echo " Your OS appears to be: " lsb_release -a
+	echo
+	echo " Your OS is not currently supported by this script ... "
+	echo
+	echo " Exiting the install. "
+	exit
+fi
+
+###########################################
+# Run a OS and Platform compatabilty Check
+###########################################
+########
+# ARMEL
+########
+case $(uname -m) in armv[4-5]l)
+echo
+echo " ArmEL is currenty UnSupported "
+echo
+exit
+esac
+
+########
+# ARMHF
+########
+case $(uname -m) in armv[6-9]l)
+echo
+echo " ArmHF arm v7 v8 v9 boards supported "
+echo
+esac
+
+#############
+# Intel/AMD
+#############
+case $(uname -m) in x86_64|i[4-6]86)
+echo
+echo " Intel / Amd boards currently UnSupported"
+echo
+exit
+esac
+
+#####################################
+#Update base os with new repo in list
+#####################################
+echo ""
+echo "--------------------------------------------------------------"
+echo "Updating Raspberry Pi repository keys..."
+echo "--------------------------------------------------------------"
+echo ""
+gpg --keyserver pgp.mit.edu --recv 8B48AD6246925553 
+gpg --export --armor 8B48AD6246925553 | apt-key add -
+gpg --keyserver pgp.mit.edu --recv  7638D0442B90D010
+gpg --export --armor  7638D0442B90D010 | apt-key add -
+gpg --keyserver pgp.mit.edu --recv CBF8D6FD518E17E1
+gpg --export --armor CBF8D6FD518E17E1 | apt-key add -
+wget https://www.raspberrypi.org/raspberrypi.gpg.key
+gpg --import raspberrypi.gpg.key | apt-key add -
+wget https://archive.raspbian.org/raspbian.public.key
+gpg --import raspbian.public.key | apt-key add -
+for i in update upgrade clean ;do apt-get -y --force-yes "${i}" ; done
+
+#####################################
+#Update base os with new repo in list
+#####################################
+apt-get update
+
+###################
+# Notes / Warnings
+###################
+echo
+cat << DELIM
+                   Not Ment For L.a.m.p Installs
+
+                  L.A.M.P = Linux Apache Mysql PHP
+
+                 THIS IS A ONE TIME INSTALL SCRIPT
+
+             IT IS NOT INTENDED TO BE RUN MULTIPLE TIMES
+
+         This Script Is Ment To Be Run On A Fresh Install Of
+
+                         Debian 8 (Jessie)
+
+     If It Fails For Any Reason Please Report To kb3vgw@gmail.com
+
+   Please Include Any Screen Output You Can To Show Where It Fails
+
+DELIM
+
+###############################################################################################
+#Testing for internet connection. Pulled from and modified
+#http://www.linuxscrew.com/2009/04/02/tiny-bash-scripts-check-internet-connection-availability/
+###############################################################################################
+echo
+echo "This Script Currently Requires a internet connection "
+echo
+wget -q --tries=10 --timeout=5 http://www.google.com -O /tmp/index.google &> /dev/null
+
+if [ ! -s /tmp/index.google ];then
+	echo "No Internet connection. Please check ethernet cable"
+	/bin/rm /tmp/index.google
+	exit 1
+else
+	echo "I Found the Internet ... continuing!!!!!"
+	/bin/rm /tmp/index.google
+fi
+echo
+printf ' Current ip is : '; ip -f inet addr show dev eth0 | sed -n 's/^ *inet *\([.0-9]*\).*/\1/p'
+echo
+
+#####################
+#ModProbe moules
+####################
+modprobe w1-gpio
+modprobe w1-therm
+
+######################
+# Enable the spi & i2c
+######################
+echo "w1-gpio" >> /etc/modules
+echo "w1-therm" >> /etc/modules
+
+##############################
+#Set a reboot if Kernel Panic
+##############################
+cat > /etc/sysctl.conf << DELIM
+kernel.panic = 10
+DELIM
+
+####################################
+# Set fs to run in a tempfs ramdrive
+####################################
+cat >> /etc/fstab << DELIM
+tmpfs /tmp  tmpfs nodev,nosuid,mode=1777  0 0
+tmpfs /var/tmp  tmpfs nodev,nosuid,mode=1777  0 0
+tmpfs /var/cache/apt/archives tmpfs   size=100M,defaults,noexec,nosuid,nodev,mode=0755 0 0
+DELIM
+
+############################
+# set usb power level
+############################
+cat >> /boot/config.txt << DELIM
+
+#usb max current
+usb_max_current=1
+DELIM
+
+###############################
+# Disable the dphys swap file
+# Extend life of sd card
+###############################
+swapoff --all
+apt-get -y remove dphys-swapfile
+rm -rf /var/swap
+
+#####################################################
+#fix usb sound/nic issue so network interface gets IP
+#####################################################
+cat > /etc/network/interfaces << DELIM
+auto lo eth0
+iface lo inet loopback
+iface eth0 inet dhcp
+
+DELIM
+
+
+#####################################
+#Update base os with new repo in list
+#####################################
+echo ""
+echo "--------------------------------------------------------------"
+echo "Updating Raspberry Pi repository keys..."
+echo "--------------------------------------------------------------"
+echo ""
+gpg --keyserver pgp.mit.edu --recv 8B48AD6246925553 
+gpg --export --armor 8B48AD6246925553 | apt-key add -
+gpg --keyserver pgp.mit.edu --recv  7638D0442B90D010
+gpg --export --armor  7638D0442B90D010 | apt-key add -
+gpg --keyserver pgp.mit.edu --recv CBF8D6FD518E17E1
+gpg --export --armor CBF8D6FD518E17E1 | apt-key add -
+wget https://www.raspberrypi.org/raspberrypi.gpg.key
+gpg --import raspberrypi.gpg.key | apt-key add -
+wget https://archive.raspbian.org/raspbian.public.key
+gpg --import raspbian.public.key | apt-key add -
+for i in update upgrade clean ;do apt-get -y --force-yes "${i}" ; done
+
+################################################################################################
+# Setting apt_get to use the httpredirecter to get
+# To have <APT> automatically select a mirror close to you, use the Geo-ip redirector in your
+# sources.list "deb http://httpredir.debian.org/debian/ jessie main".
+# See http://httpredir.debian.org/ for more information.  The redirector uses HTTP 302 redirects
+# not dnS to serve content so is safe to use with Google dnS.
+# See also <which httpredir.debian.org>.  This service is identical to http.debian.net.
+#################################################################################################
+cat > "/etc/apt/sources.list" << DELIM
+deb http://httpredir.debian.org/debian/ jessie main contrib non-free
+deb-src http://httpredir.debian.org/debian/ jessie main contrib non-free
+deb http://httpredir.debian.org/debian/ jessie-updates main contrib non-free
+deb-src http://httpredir.debian.org/debian/ jessie-updates main contrib non-free
+deb http://httpredir.debian.org/debian/ jessie-backports main contrib non-free
+deb-src http://httpredir.debian.org/debian/ jessie-backports main contrib non-free
+
+DELIM
+
+############
+# Raspi Repo
+###########################################################################
+# Put in Proper Location. All addon repos should be source.list.d sub dir
+###########################################################################
+cat > /etc/apt/sources.list.d/raspi.list << DELIM
+deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib firmware non-free rpi
+DELIM
+
+#############################
+# SvxLink Release Repo ArmHF
+#############################
+cat > "/etc/apt/sources.list.d/svxlink.list" <<DELIM
+deb http://104.236.193.157/svxlink/release/debian/ jessie main
+DELIM
+
+######################
+#Update base os
+######################
+for i in update upgrade clean ;do apt-get -y "${i}" ; done
+
+##########################
+#Installing svxlink Deps
+##########################
+apt-get install -y sqlite3 libopus0 alsa-utils vorbis-tools sox libsox-fmt-mp3 librtlsdr0 \
+		ntp libasound2 libspeex1 libgcrypt20 libpopt0 libgsm1 tcl8.6 tk8.6 alsa-base bzip2 \
+		sudo gpsd gpsd-clients flite wvdial inetutils-syslogd screen time uuid vim install-info \
+		usbutils whiptail dialog logrotate cron gawk watchdog python3-serial network-manager \
+		git-core wiringpi python-pip libsigc++-2.0-0c2a
+
+#################
+#Install svxlink
+#################
+apt-get -y --force-yes install svxlink-server remotetrx
+
+#cleanup
+apt-get clean
+
+############################################
+#Backup Basic svxlink original config files
+############################################
+mkdir -p /usr/share/examples/svxlink/conf
+cp -rp /etc/svxlink/* /usr/share/examples/svxlink/conf
+
+#adding user svxlink to gpio user group
+usermod -a -G gpio svxlink
+
+##############################
+#Install Courtesy Sound Files
+##############################
+wget --no-check-certificate https://github.com/kb3vgw/Svxlink-Courtesy_Tones/archive/15.10.1.tar.gz
+tar xzvf 15.10.1.tar.gz
+mv Svxlink-Courtesy_Tones-15.10.1 Courtesy_Tones
+mv Courtesy_Tones /usr/share/svxlink/sounds/
+rm 15.10.1.tar.gz
+
+################################
+#Make and Link Custome Sound Dir
+################################
+mkdir -p /root/sounds/Custom_Courtesy_Tones
+ln -s /root/sounds/Custom_Courtesy_Tones /usr/share/svxlink/sounds/Custom_Courtesy_Tones
+mkdir -p /root/sounds/Custom_Identification
+ln -s /root/sounds/Custom_identification /usr/share/svxlink/sounds/Custom_Identification
+
+#################################
+# Make and link Local event.d dir
+#################################
+mkdir /etc/svxlink/local-events.d
+ln -s /etc/svxlink/local-events.d /usr/share/svxlink/events.d/local
+
+###########################
+#Install Custom Logic Files
+###########################
+git clone https://github.com/kb3vgw/Svxlink-Custom-Logic.git
+cp -rp Svxlink-Custom-Logic/* /etc/svxlink/local-events.d
+rm -rf Svxlink-Custom-Logic
+
+###########################################################
+#Disable onboard hdmi soundcard not used in openrepeater
+#/boot/config.txt and /etc/modules
+###########################################################
+#/boot/config.txt
+sed -i /boot/config.txt -e"s#dtparam=audio=on#\#dtparam=audio=on#"
+
+#/etc/modules
+sed -i /etc/modules -e"s#snd-bcm2835#\#snd-bcm2835#"
+
+################################
+#Set up usb sound for alsa mixer
+################################
+if ( ! `grep "snd-usb-audio" /etc/modules >/dev/null`) ; then
+   echo "snd-usb-audio" >> /etc/modules
+fi
+FILE=/etc/modprobe.d/alsa-base.conf
+sed "s/options snd-usb-audio index=-2/options snd-usb-audio index=0/" $FILE > ${FILE}.tmp
+mv -f ${FILE}.tmp ${FILE}
+if ( ! `grep "options snd-usb-audio nrpacks=1" ${FILE} > /dev/null` ) ; then
+  echo "options snd-usb-audio nrpacks=1 index=0" >> ${FILE}
+fi
+
+######################
+#Install svxlink Menu
+#####################
+git clone https://github.com/kb3vgw/svxlink-menu.git
+chmod +x svxlink-menu/svxlink_config
+cp -r svxlink-menu/svxlink_config /usr/bin
+rm -rf svxlink-menu
+
+##############################################
+# Enable New shellmenu for logins  on enabled 
+# for root and only if the file exist
+##############################################
+cat >> /root/.profile << DELIM
+
+if [ -f /usr/bin/svxlink_config ]; then
+        . /usr/bin/svxlink_config
+fi
+
+DELIM
+
+#######################
+#Enable Systemd Service
+####################### 
+echo " Enabling the Svxlink systemd Service Daemon "
+systemctl enable svxlink.service
+
+#######################
+#Enable Systemd Service
+####################### 
+echo " Enabling the Svxlink Remotetrx systemd Service Daemon "
+systemctl enable remotetrx.service
+
+############################################
+#reboot sysem for all changes to take effect
+############################################
+echo " rebooting system forfull changes to take effect "
+reboot
+
+) | tee /root/install.log

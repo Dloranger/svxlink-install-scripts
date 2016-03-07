09bafe61b287a0712876d30cff08fdce9f2119bc
Richard Neese <r.neese@gmail.com>
2016-03-07 10:36:30 -0600

    fixed hardkernel install

--- ODROID-C1-Universal-Build-Script/HardKernel-Odroid-Jessie-SvxLink-Release-embedded-pkg-install-english.sh
+++ ODROID-C1-Universal-Build-Script/HardKernel-Odroid-Jessie-SvxLink-Release-embedded-pkg-install-english.sh
@@ -125,7 +125,7 @@ for i in update upgrade clean ;do apt-get -y "${i}" ; done
 #######################
 #Install Dependancies
 #######################
-apt-get install -y sqlite3 libopus0 alsa-base alsa-utils vorbis-tools sox libsox-fmt-mp3 
+apt-get install -y sqlite3 libopus0 alsa-base alsa-utils vorbis-tools sox libsox-fmt-mp3 \
         librtlsdr0 ntp libasound2 libspeex1 libgcrypt20 libpopt0 libgsm1 tcl8.6 tk8.6 bzip2 \
         gpsd gpsd-clients flite wvdial inetutils-syslogd screen vim usbutils logrotate cron \
         gawk python3-serial git-core python-pip libsigc++-2.0-0c2a libhamlib2 libhamlib2++c2 \
